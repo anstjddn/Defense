94c719648e14d4c7667f30e57ad613a7ee7504c9
VR <mxj1234@naver.com>
2023-06-05 21:33:39 +0900

    docs: 6.5 수업및 과제

--- Assets/Scripts/TowerPlace.cs
+++ Assets/Scripts/TowerPlace.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.HID;
 using UnityEngine.UIElements;
 
-public class TowerPlace : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IDragHandler
+public class TowerPlace : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IDragHandler, IEndDragHandler
 {
     [SerializeField] Color normal;
     [SerializeField] Color OnMouse;
+    [SerializeField] Color OnPlace;
 
     private Renderer render;
-
+    private Transform prepoint;
     private void Awake()
     {
+        prepoint = GetComponent<Transform>();
         render = GetComponent<Renderer>();
     }
 
@@ -41,8 +46,21 @@ public class TowerPlace : MonoBehaviour, IPointerClickHandler, IPointerEnterHand
 
     public void OnDrag(PointerEventData eventData)
     {
-       
+        render.material.color = OnMouse;
+        transform.position += new Vector3(eventData.delta.x, 0, eventData.delta.y);
     }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        render.material.color = normal;
+    }
+    public bool GroundCheck()
+    {
+        RaycastHit hit;
+
+        return Physics.SphereCast(transform.position + Vector3.up * 1, 0.5f, Vector3.down, out hit, 0.6f);
+    }
+
 
 }
 
